bd42fe128922aaa6022306452d5e967e36c2c9ef
yosefnega <nega_yosef@yahoo.com>
2021-05-21 14:25:02 +0300

    complete post build script

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,32 +1,42 @@
 #!/usr/bin/env bash
 set -e
 
-ls -a $APPCENTER_SOURCE_DIRECTORY
-find $APPCENTER_OUTPUT_DIRECTORY
+# APP_NAME: <username>/<app name>
+echo $APP_NAME
+# DEVICE_SET: <username>/<device set>
+echo $DEVICE_SET
+# TEST_SERIES: <test series>
+echo $TEST_SERIES
 
 
-# TEST_DIRECTORY=$APPCENTER_SOURCE_DIRECTORY/test
-# APP_PATH=$APPCENTER_OUTPUT_DIRECTORY/android/*.apk
-# SCRIPTS_DIRECTORY=$APPCENTER_SOURCE_DIRECTORY/scripts
-# APP_NAME="nega_yosef/sample_android"
-# DEVICES="nega_yosef/new-devices"
-# TEST_SERIES="launch-tests"
-# LOCALE="en_US"
+TEST_DIRECTORY=$APPCENTER_SOURCE_DIRECTORY/test
+TEST_BUILD_DIR=$TEST_DIRECTORY/target/upload
+APP_PATH=$APPCENTER_OUTPUT_DIRECTORY/*.apk
+SCRIPTS_DIRECTORY=$APPCENTER_SOURCE_DIRECTORY/scripts
+LOCALE="en_US"
 
-# # check if build succeded 
-# if find $APPCENTER_SOURCE_DIRECTORY -name '*.UITest.csproj';
-# then
-#     #build test files using mvn
-#     source run_tests.sh
+JOB_STATUS=$AGENT_JOBSTATUS
+# check if build succeded 
+if [ "$AGENT_JOBSTATUS" = "Succeeded" ]; then 
+    echo "build junit tests"
+    cd $TEST_DIRECTORY && mvn -DskipTests -P prepare-for-upload package
+    cd $APPCENTER_SOURCE_DIRECTORY
+
+    echo "schedule appium test run"
+    appcenter test run appium  \
+    --app $APP_NAME \
+    --devices $DEVICES \
+    --app-path $APP_PATH \
+    --test-series $TEST_SERIES \
+    --locale $LOCALE  \
+    --token $APPCENTER_TOKEN \
+    --build-dir $TEST_BUILD_DIR \
+    --async
 
 
+else
 
+    echo "build failed"
+    exit 1
 
-# appcenter test run appium  \
-#     --app $APP_NAME \
-#     --devices $DEVICES \
-#     --app-path $APP_PATH \
-#     --test-series $TEST_SERIES \
-#     --locale $LOCALE  \
-#     --token $APPCENTER_TOKEN \
-#     --build-dir target/upload
+fi
